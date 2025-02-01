4462d2a635b9cba8455bd5ed12495818a506b3d1
Shakilofficial <mrshakilhossainofficial@gmail.com>
2025-02-01 18:41:23 +0600

    update added duynamic route and serch params

---
+++ src/app/(dashboardLayout)/dashboard/profile/settings/page.tsx
@@ -0,0 +1,9 @@
+const SettingsPage = () => {
+  return (
+    <div>
+      <h1>This is the Settings page component from the Dashboard Profile</h1>
+    </div>
+  );
+};
+
+export default SettingsPage;
---
+++ src/app/(mainLayout)/products/product/page.tsx
@@ -0,0 +1,9 @@
+const ProductDetailsPage = () => {
+  return (
+    <div>
+      <h1>This is the Product Detail page component</h1>
+    </div>
+  );
+};
+
+export default ProductDetailsPage;
