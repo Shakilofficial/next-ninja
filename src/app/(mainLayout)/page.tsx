c2a55a5666ae0371236bfc58197a8eea45b0e9ba
Shakilofficial <mrshakilhossainofficial@gmail.com>
2025-02-01 19:18:09 +0600

    module wrap up

--- src/app/(mainLayout)/page.tsx
+++ src/app/(mainLayout)/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/Hero";
 
 const MainPage = () => {
+  console.log("Hello World"); // This is server component, this log will not be shown in the browser, it will only be shown in the terminal
   return (
     <div>
       <Hero />
---
+++ src/app/(mainLayout)/products/[...slug]/page.tsx
@@ -0,0 +1,9 @@
+const CatchAllProductDetailsPage = () => {
+  return (
+    <div>
+      <h1>This is the Catch All Product Detail page component</h1>
+    </div>
+  );
+};
+
+export default CatchAllProductDetailsPage;
