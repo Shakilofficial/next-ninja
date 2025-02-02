326f770cdf1a1a1613c62a6dbfe70696fe2bc380
Shakilofficial <mrshakilhossainofficial@gmail.com>
2025-02-03 00:15:48 +0600

    implement fetch method both SSR and SSG

--- src/app/(mainLayout)/page.tsx
+++ src/app/(mainLayout)/page.tsx
@@ -1,9 +1,45 @@
 import Hero from "@/components/Hero";
-
-const MainPage = () => {
+import Link from "next/link";
+interface Product {
+  id: string;
+  title: string;
+  description: string;
+  price: number;
+  category: string;
+  quantity: number;
+  inStock: boolean;
+}
+const MainPage = async () => {
+  const res = await fetch("http://localhost:5000/products", {
+    next: { revalidate: 60 },
+  });
+  const prodtucts = await res.json();
+ 
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center mb-8">
       <Hero />
+      <div className="grid grid-cols-3 gap-4 max-w-[90%] mx-auto my-10">
+        {prodtucts.slice(0, 3).map((prod: Product) => {
+          return (
+            <div
+              className="border border-slate-400 rounded-lg p-4"
+              key={prod.id}
+            >
+              <h2>{prod.title}</h2>
+              <p>{prod.description}</p>
+              <p>Price: {prod.price}</p>
+              <p>Category: {prod.category}</p>
+              <p>Quantity: {prod.quantity}</p>
+              <p>In Stock: {prod.inStock ? "Yes" : "No"}</p>
+            </div>
+          );
+        })}
+      </div>
+      <Link href="/products">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          View All Products
+        </button>
+      </Link>
     </div>
   );
 };
--- src/app/(mainLayout)/products/[productId]/page.tsx
+++ src/app/(mainLayout)/products/[productId]/page.tsx
@@ -1,24 +1,9 @@
-interface Props {
-  params: {
-    productId: string;
-  };
-  searchParams: {
-    [key: string]: string;
-  };
-}
-
-const DyanamicProductDetailsPage = async ({ params, searchParams }: Props) => {
-  const { productId } = await params;
-  console.log(await searchParams);
-
+const ProductPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center my-6">
-      <h1 className="text-2xl text-blue-500">
-        This is the Dynamic Product Detail page component for Product ID:
-        {productId}
-      </h1>
+    <div>
+      <h1>This is the Product page component</h1>
     </div>
   );
 };
 
-export default DyanamicProductDetailsPage;
+export default ProductPage;
